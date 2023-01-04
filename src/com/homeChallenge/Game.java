e6e1a364a4744d6cce8665d1a35c386c3dbe6cfe
thaodev <71045132+thaodev@users.noreply.github.com>
2023-01-04 11:23:57 +0700

    Complete taking user input

--- src/com/homeChallenge/Game.java
+++ src/com/homeChallenge/Game.java
@@ -1,10 +1,34 @@
 package com.homeChallenge;
 
+import java.util.Scanner;
+
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {3, 4, 5, 3};
+		
+	}
+	
+	public static int[] playerInput() {
+		Scanner sc = new Scanner(System.in);
+		int[] playerNum = new int[4];
+		int count = 0;
+		System.out.println("Enter your guess: ");
+		while(count < 4) {
+			playerNum[count] = sc.nextInt();
+			count++;
+		}
+		return playerNum;
+	}
+	
+	public static void printPlayerInput(int[] arr) {
+		for(int i : arr) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void gameAlgorithm(int[] arr1, int[] arr2) {
+		
 	}
 
 }
